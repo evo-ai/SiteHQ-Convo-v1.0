a68befeda1ccb66ac18e0f3708cb83f79ce038ed
curiosityspace7 <39021585-curiosityspace7@users.noreply.replit.com>
2025-01-25 05:20:46 +0000

    Agent query: Is the test page accessible and showing the embedding instructions with a demo iframe?
    Serve static files from client/public directory.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/405298aa-2204-458a-a972-de641dee2e38/d5b12ea5-d567-46e4-a5d3-0a4171a7e300.jpg

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Serve widget.js as static file
+// Serve static files from client/public directory
 app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.use((req, res, next) => {
